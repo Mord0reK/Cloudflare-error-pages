5fcb0ccd9afe0efa5eb468cc9e8f9dfcd0d73a6b
Mord0reK <135718526+Mord0reK@users.noreply.github.com>
2026-03-08 12:58:40 +0100

    feat: remove default error page handling and update fallback response

--- src/worker.js
+++ src/worker.js
@@ -2,7 +2,6 @@
 // (Dzięki regułom w wrangler.toml Cloudflare wczyta je jako zwykły tekst)
 import html502 from "../dist/src/pages/502/index.html";
 import html1002 from "../dist/src/pages/1002/index.html";
-import htmlDefault from "../dist/src/pages/default/index.html";
 
 export default {
   async fetch(request, env, ctx) {
@@ -21,19 +20,13 @@ export default {
           headers: { "Content-Type": "text/html;charset=UTF-8" }
         });
       }
-      if (response.status >= 500 && response.status <= 599) {
-        return new Response(htmlDefault, {
-          status: response.status,
-          headers: { "Content-Type": "text/html;charset=UTF-8" }
-        });
-      }
       return response;
 
     } catch (error) {
       
       console.error("Błąd połączenia z serwerem origin:", error); // Będzie widoczne w logach Cloudflare
 
-      return new Response(htmlDefault, {
+      return new Response(html1002, {
         status: 502,
         headers: { "Content-Type": "text/html;charset=UTF-8" }
       });
